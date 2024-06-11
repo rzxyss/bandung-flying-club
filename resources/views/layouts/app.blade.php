24ddfbba53a89e1cf91868d8c995d1cc977adcbd
Rizki Saepul Aziz <rzkysa0@gmail.com>
2024-06-11 09:45:11 +0700

    fix responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                 </svg>
             </button>
             <div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-                <ul class="flex flex-colarl mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
+                <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
                     <li>
                         <a href="/"
                             class="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
