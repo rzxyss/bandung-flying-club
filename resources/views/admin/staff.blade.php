bf291eb47435929c7a148b65d1d1fd72ec0e674b
Rizki Saepul Aziz <rzkysa0@gmail.com>
2024-06-12 23:43:52 +0700

    add staff page

--- resources/views/admin/staff.blade.php
+++ resources/views/admin/staff.blade.php
@@ -6,10 +6,11 @@
 
             <h1 class="font-semibold text-2xl">Staff Bandung Flying Club</h1>
         </div>
-        <a href="#" class="flex items-center gap-1 bg-[#88CCA7] p-5 rounded-full text-white">
+        <button data-modal-target="staff-modal" data-modal-toggle="staff-modal"
+            class="flex items-center gap-1 bg-[#88CCA7] p-5 rounded-full text-white">
             <i class="fa-solid fa-plus font-semibold text-xl"></i>
             <h1 class="font-semibold font-xl">Tambah</h1>
-        </a>
+        </button>
     </div>
     <div class="grid md:grid-cols-3 gap-4">
         {{-- Start Looping Staff --}}
@@ -41,4 +42,67 @@
         </div>
         {{-- End Looping Staff --}}
     </div>
+
+    {{-- Modal Tambah Staff --}}
+    <div id="staff-modal" tabindex="-1" aria-hidden="true"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-2xl max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Tambah Staff
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="staff-modal">
+                        <i class="fa-solid fa-x"></i>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <form>
+                    <div class="p-4 md:p-5 space-y-4">
+                        <div class="mb-5">
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                            <input type="text"
+                                class="bg-[#EBEBEB] border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5"
+                                required />
+                        </div>
+                        <div class="mb-5">
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal
+                                Lahir</label>
+                            <input type="text"
+                                class="bg-[#EBEBEB] border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5"
+                                required />
+                        </div>
+                        <div class="mb-5">
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
+                            <input type="text"
+                                class="bg-[#EBEBEB] border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5"
+                                required />
+                        </div>
+                        <div class="mb-5">
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">No Handphone</label>
+                            <input type="text"
+                                class="bg-[#EBEBEB] border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5"
+                                required />
+                        </div>
+                        <div class="mb-5">
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foto</label>
+                            <input type="file"
+                                class="bg-[#EBEBEB] border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5"
+                                required />
+                        </div>
+                    </div>
+
+                    <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600 gap-2">
+                        <button
+                            class="text-white bg-green-700 hover:bg-green-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
+                        <button data-modal-hide="staff-modal" type="button"
+                            class="text-white bg-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- End Modal Tambah Staff --}}
 @endsection
