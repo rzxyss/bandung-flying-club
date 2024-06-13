4a333425685f67e1107f3465eef7671d51c9dcc3
Rizki Saepul Aziz <rzkysa0@gmail.com>
2024-06-13 11:46:51 +0700

    fix responsive design

--- resources/views/admin/ticketing.blade.php
+++ resources/views/admin/ticketing.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="flex flex-col md:flex-row md:items-start gap-2">
-        <div class="w-full md:w-4/12">
-            <h1 class="font-medium text-lg md:pb-3">Input Ketersediaan Jadwal</h1>
-            <div class="relative overflow-x-auto w-full">
+    <div class="relative overflow-x-clip w-full">
+        <div class="flex flex-col md:flex-row md:items-start gap-2">
+            <div class="w-full md:w-4/12">
+                <h1 class="font-medium text-lg md:pb-3">Input Ketersediaan Jadwal</h1>
                 <form>
                     <div class="mb-5">
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal</label>
@@ -47,64 +47,65 @@
                     </div>
                 </form>
             </div>
+
+            {{-- Jadwal Section --}}
+            <div class="w-full md:w-4/12 flex flex-col gap-2 pt-10 md:pt-0 overflow-x-auto">
+                <h1 class="hidden md:flex font-medium text-lg md:pb-3">Jadwal Tersedia</h1>
+                <button class="flex justify-between md:hidden button-jadwal">
+                    <h1 class="font-medium text-lg">Jadwal Tersedia</h1>
+                    <i class="fa-solid fa-angle-down"></i>
+                </button>
+                <div class="jadwal hidden md:flex flex-col gap-1">
+                    {{-- Loop Jadwal --}}
+                    <div class="relative w-80 md:w-96 p-4 bg-[#EBEBEB] rounded-lg shadow-md overflow-hidden">
+                        <div class="absolute inset-0">
+                            <div class="w-1/3 h-full bg-[#77C5C5] skew-x-[-20deg] origin-top"></div>
+                        </div>
+                        <div class="relative flex items-center">
+                            <div class="md:p-2 lg:p-4 text-white z-10">
+                                <p class="font-bold">10.30</p>
+                                <p class="font-bold">11.00</p>
+                            </div>
+                            <div class="relative z-10 text-gray-600 ml-11 md:ml-10 flex-1">
+                                <p>Sulaiman - Std Jalak Harupat - Kota Soreang - Telkom - Sulaiman</p>
+                                <p class="text-center mt-4">20 - 04 - 2024</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Loop --}}
+                </div>
+            </div>
+            {{-- End Jadwal Section --}}
+
+            {{-- Konfirmasi Pembayaran Section --}}
+            <div class="w-full md:w-4/12 flex flex-col gap-2 pt-10 md:pt-0 overflow-x-auto">
+                <h1 class="hidden md:flex font-medium text-lg md:pb-3">Konfirmasi Pembayaran</h1>
+                <button class="flex justify-between md:hidden button-konfirmasi">
+                    <h1 class="font-medium text-lg">Konfirmasi Pembayaran</h1>
+                    <i class="fa-solid fa-angle-down"></i>
+                </button>
+                <div class="konfirmasi hidden md:flex flex-col gap-1">
+                    {{-- Loop Konfirmasi Pembayaran --}}
+                    <div class="relative w-80 md:w-96 p-4 bg-[#EBEBEB] rounded-lg shadow-md overflow-hidden">
+                        <div class="absolute inset-0">
+                            <div class="w-1/3 h-full bg-[#C5BD77] skew-x-[-20deg] origin-top"></div>
+                        </div>
+                        <div class="relative flex items-center">
+                            <div class="md:p-2 lg:p-4 text-white z-10">
+                                <p class="font-bold">10.30</p>
+                                <p class="font-bold">11.00</p>
+                            </div>
+                            <div class="relative z-10 text-gray-600 ml-11 md:ml-10 flex-1">
+                                <p>Sulaiman - Std Jalak Harupat - Kota Soreang - Telkom - Sulaiman</p>
+                                <p class="text-center mt-4">20 - 04 - 2024</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- End Loop --}}
+                </div>
+            </div>
+            {{-- End Konfirmasi Pembayaran Section --}}
         </div>
 
-        {{-- Jadwal Section --}}
-        <div class="w-full md:w-4/12 flex flex-col gap-2 pt-10 md:pt-0">
-            <h1 class="hidden md:flex font-medium text-lg md:pb-3">Jadwal Tersedia</h1>
-            <button class="flex justify-between md:hidden button-jadwal">
-                <h1 class="font-medium text-lg">Jadwal Tersedia</h1>
-                <i class="fa-solid fa-angle-down"></i>
-            </button>
-            <div class="jadwal hidden md:flex flex-col gap-1">
-                {{-- Loop Jadwal --}}
-                <div class="relative w-80 md:w-96 p-4 bg-[#EBEBEB] rounded-lg shadow-md overflow-hidden">
-                    <div class="absolute inset-0">
-                        <div class="w-1/3 h-full bg-[#77C5C5] skew-x-[-20deg] origin-top"></div>
-                    </div>
-                    <div class="relative flex items-center">
-                        <div class="md:p-4 text-white z-10">
-                            <p class="font-bold">10.30</p>
-                            <p class="font-bold">11.00</p>
-                        </div>
-                        <div class="relative z-10 text-gray-600 ml-11 md:ml-10 flex-1">
-                            <p>Sulaiman - Std Jalak Harupat - Kota Soreang - Telkom - Sulaiman</p>
-                            <p class="text-center mt-4">20 - 04 - 2024</p>
-                        </div>
-                    </div>
-                </div>
-                {{-- End Loop --}}
-            </div>
-        </div>
-        {{-- End Jadwal Section --}}
-
-        {{-- Konfirmasi Pembayaran Section --}}
-        <div class="w-full md:w-4/12 flex flex-col gap-2 pt-10 md:pt-0">
-            <h1 class="hidden md:flex font-medium text-lg md:pb-3">Konfirmasi Pembayaran</h1>
-            <button class="flex justify-between md:hidden button-konfirmasi">
-                <h1 class="font-medium text-lg">Konfirmasi Pembayaran</h1>
-                <i class="fa-solid fa-angle-down"></i>
-            </button>
-            <div class="konfirmasi hidden md:flex flex-col gap-1">
-                {{-- Loop Konfirmasi Pembayaran --}}
-                <div class="relative w-80 md:w-96 p-4 bg-[#EBEBEB] rounded-lg shadow-md overflow-hidden">
-                    <div class="absolute inset-0">
-                        <div class="w-1/3 h-full bg-[#C5BD77] skew-x-[-20deg] origin-top"></div>
-                    </div>
-                    <div class="relative flex items-center">
-                        <div class="md:p-4 text-white z-10">
-                            <p class="font-bold">10.30</p>
-                            <p class="font-bold">11.00</p>
-                        </div>
-                        <div class="relative z-10 text-gray-600 ml-11 md:ml-10 flex-1">
-                            <p>Sulaiman - Std Jalak Harupat - Kota Soreang - Telkom - Sulaiman</p>
-                            <p class="text-center mt-4">20 - 04 - 2024</p>
-                        </div>
-                    </div>
-                </div>
-                {{-- End Loop --}}
-            </div>
-        </div>
-        {{-- End Konfirmasi Pembayaran Section --}}
     </div>
 @endsection
