6cd580746e0ee6cc1cea0ab3f741cb179f0e7612
Rizki Saepul Aziz <rzkysa0@gmail.com>
2024-06-14 16:49:16 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,18 @@ Route::get('/', function () {
 Route::get('/news', function () {
     return view('news');
 });
+Route::get('/news/detail', function () {
+    return view('news_detail');
+});
+Route::get('/armada/detail', function () {
+    if (request()->has('ct2k')) {
+        return view('detail_ct2k');
+    } else if(request()->has('savanah')) {
+        return view('detail_savanah');
+    } else if(request()->has('zenitstol')) {
+        return view('detail_zenitstol');
+    }
+});
 Route::get('/pilih_paket', function () {
     if (request()->has('isi_detail')) {
         return view('detail');
@@ -36,11 +48,23 @@ Route::prefix('admin')->group(function () {
     Route::get('/staff', function () {
         return view('admin.staff');
     });
+    Route::get('/staff/add', function () {
+        return view('admin.add_staff');
+    });
+    Route::get('/staff/edit', function () {
+        return view('admin.edit_staff');
+    });
     Route::get('/ticketing', function () {
         return view('admin.ticketing');
     });
     Route::get('/inventory', function () {
         return view('admin.inventory');
+    });
+    Route::get('/inventory/add', function () {
+        return view('admin.add_inventory');
+    });
+    Route::get('/inventory/edit', function () {
+        return view('admin.edit_inventory');
     });
     Route::get('/maintenance', function () {
         return view('admin.maintenance');
